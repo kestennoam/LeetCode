a78b76dc3dd7532aa1d49b50d92aa9d761f4c547
noamkesten <noamkesten@gmail.com>
2020-08-13 01:36:16 +0300

    Done with O(1) space

--- August_Challnge/12-Pacal's_Triangle_II.py
+++ August_Challnge/12-Pacal's_Triangle_II.py
@@ -1,6 +1,13 @@
 class Solution:
     def getRow(self, rowIndex: int):
-        rows=[[1]]
+        rows = [[1]]
         for i in range(rowIndex):
-            rows.append([1]+[sum(pair) for pair in zip(rows[-1],rows[-1][1:])]+[1])
-        return rows[-1]
+            rows = [[1]
+                    + [sum(pair) for pair in zip(rows[-1], rows[-1][1:])] +
+                    [1]]
+        return rows[0]
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.getRow(4))
